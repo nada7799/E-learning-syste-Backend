8c7baaa7bc7b31f2a285b2197730099170ab6905
nada7799 <91373012+nada7799@users.noreply.github.com>
2024-02-07 20:23:55 +0200

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { config } from "./config/config";
+import {app} from "./app";
+import express from 'express';
+mongoose.connect(config.mongo.url).then(()=>
+{
+    console.log("connected");
+}
+).catch((error)=>{
+    console.log(error);
+});
+
+app.listen(config.server.port,() =>{
+    console.log("listening on port "+ config.server.port);
+});
